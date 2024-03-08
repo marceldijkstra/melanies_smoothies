e37ab94c14d81f680274a2f6a891032ee9c551c0
marceldijkstra <162569853+marceldijkstra@users.noreply.github.com>
2024-03-08 08:33:34 +0100

    Update streamlit_app.py
    added fruityvice request

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 	
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+# New section to display fruityfice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
